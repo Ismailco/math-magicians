50f4d6c7542df3567cd43d67a27acb1c0403f531
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-04-17 23:58:33 +0000

    Fix Page responsive  - Fix footer to stick to bottom of the page  - Make the calculatore responsive in mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="footer">
+    <div className="footer" data-role="footer" data-position="fixed">
       <p className="copy">
         All right Reserved.
         <br />
